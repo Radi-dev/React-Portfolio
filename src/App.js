3723833322dbf278b55deaa8fc8c3a761537d8cf
Radi <evaradi18@gmail.com>
2021-12-14 09:43:53 +0100

    App.js, tailwind.css

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./Tailwind.css";
+/*import Home from "./Components/Home";
+import Header from "./Components/Header";
+import Setup from "./Components/Setup";
+import About from "./Components/About";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Blog from "./Components/Blog/blogHome";*/
+import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+
+/*export default function App() {
+  const pagejsx = (
+    <div className="mother h-full font-Monte  bg-bl ue-200 px-5 py-12" id="top">
+      <div className="page-conatainer h-full mx-auto w-full relative overflow-y- scroll snap snap-y snap-mandatory">
+        <Header />
+        <Setup />
+        <Home id="home" />
+        <Projects id="projects" />
+        <About id="about" />
+        <Contact id="contact" />
+        <Footer />
+      </div>
+    </div>
+  );
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={pagejsx} />
+          <Route path="/blog" element={<Blog />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+*/
