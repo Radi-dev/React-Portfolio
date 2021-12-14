4fe44268b2f68b7da2c1f24ace0f8d808ee133a1
Radi <evaradi18@gmail.com>
2021-12-14 09:32:42 +0100

    Added dependencies. Setup tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+    "./src/components/*.{js,jsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      height: {
+        screen90: "85vh",
+      },
+      minHeight: {
+        "1/5": "3ch",
+      },
+      fontFamily: {
+        Monte: ["Montserrat", "Arial", "Helvetica", "sans-serif"],
+      },
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        orange: colors.orange,
+        paleSpringBud: "#ccd5aeff",
+        lightGoldenrodYellow: "#e9edc9ff",
+        cornsilk: "#fefae0ff",
+        papayaWhip: "#faedcdff",
+        fawn: "#d4a373ff",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
