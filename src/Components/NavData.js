ed1a646bb779a942002a46ae1c48671c784332af
Radi <evaradi18@gmail.com>
2021-12-14 09:51:44 +0100

    Added NavData

---
+++ src/Components/NavData.js
@@ -0,0 +1,23 @@
+export const NavData = [
+  {
+    title: "RADI",
+    link: "#home",
+    index: true,
+  },
+  {
+    title: "PROJECTS",
+    link: "#projects",
+  },
+  {
+    title: "ABOUT",
+    link: "#about",
+  },
+  {
+    title: "CONTACT",
+    link: "#contact",
+  },
+  {
+    title: "BLOG",
+    link: "blog",
+  },
+];
