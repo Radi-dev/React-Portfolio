fa5308ec11f1183b4c88f73b95198c3401535652
Radi <evaradi18@gmail.com>
2021-12-14 09:50:58 +0100

    Created Footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="relative h-16 shadow-2xl text-red-700 w-full rounded-b-3xl rounded bottom-0 bg-blue-400 snap-start">
+      <div className="absolute inset-y-1/2 text-center w-full">Footer</div>
+    </div>
+  );
+}
