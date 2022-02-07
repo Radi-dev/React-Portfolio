95b682f0c318e4b708edef2584802515d5fd1a4d
Radi-dev <evaradi18@gmail.com>
2022-02-07 18:07:32 +0100

    .

--- src/Components/suggested.js
+++ src/Components/suggested.js
@@ -8,10 +8,10 @@ export default function Suggested(d) {
           Recommended Articles
         </h2>
 
-        <div class="flex items-center justify-center w-full overflow-auto ">
-          <div class="flex px-4 gap-2 mt-4 w-full lg:justify-center">
-            <div class=" text-left">
-              <div class="w-48 h-48 m-2">
+        <div class="flex items-center justify-center w-full overflow-x-auto ">
+          <div class="flex px-4 gap-2 my-4 w-full lg:justify-center">
+            <div class=" text-left relative">
+              <div class="w-48 h-48 m-">
                 <img
                   class="object-cover w-full h-full object-center  mx-auto rounded-lg"
                   src="https://images.unsplash.com/photo-1493863641943-9b68992a8d07?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=739&q=80"
@@ -19,13 +19,13 @@ export default function Suggested(d) {
                 />
               </div>
 
-              <div class="mt-2 pl-2">
-                <h3 class="text-sm font-medium text-gray-700 ">Ahmed Omer</h3>
+              <div class="mt-2 pl-2 absolute bottom-0 left- px-3 bg-slate-900 bg-opacity-30 rounded-b-lg inline inset-x-0">
+                <h3 class="text-sm font-medium text-slate-50 "> Ahmed Omer</h3>
               </div>
             </div>
 
-            <div class="w-ful l ma x-w-xs text-left">
-              <div class="w-48 h-48 m-2">
+            <div class=" text-left relative">
+              <div class="w-48 h-48 m-">
                 <img
                   class="object-cover w-full h-full object-center mx-auto rounded-lg"
                   src="https://images.unsplash.com/photo-1516756587022-7891ad56a8cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
@@ -33,13 +33,13 @@ export default function Suggested(d) {
                 />
               </div>
 
-              <div class="mt-2 pl-2">
-                <h3 class="text-sm font-medium text-gray-700 ">Jane Doe</h3>
+              <div class="mt-2 pl-2 absolute bottom-0 left- px-3 bg-slate-900 bg-opacity-30 rounded-b-lg inline inset-x-0">
+                <h3 class="text-sm font-medium text-slate-50 ">Jane Doe</h3>
               </div>
             </div>
 
-            <div class="w-ful l ma x-w-xs text-left">
-              <div class="w-48 h-48 m-2">
+            <div class=" text-left relative">
+              <div class="w-48 h-48 m-">
                 <img
                   class="object-cover w-full h-full object-center mx-auto rounded-lg"
                   src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=731&q=80"
@@ -47,13 +47,13 @@ export default function Suggested(d) {
                 />
               </div>
 
-              <div class="mt-2 pl-2">
-                <h3 class="text-sm font-medium text-gray-700 ">Steve Ben</h3>
+              <div class="mt-2 pl-2 absolute bottom-0 left- px-3 bg-slate-900 bg-opacity-30 rounded-b-lg inline inset-x-0">
+                <h3 class="text-sm font-medium text-slate-50 ">Steve Ben</h3>
               </div>
             </div>
 
-            <div class="w-ful l ma x-w-xs text-left">
-              <div class="w-48 h-48 m-2">
+            <div class=" text-left relative">
+              <div class="w-48 h-48 m-">
                 <img
                   class="object-cover w-full h-full object-center mx-auto rounded-lg"
                   src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80"
@@ -61,8 +61,8 @@ export default function Suggested(d) {
                 />
               </div>
 
-              <div class="mt-2 pl-2">
-                <h3 class="text-sm font-medium text-gray-700 ">Steve Ben</h3>
+              <div class="mt-2 pl-2 absolute bottom-0 left- px-3 bg-slate-900 bg-opacity-30 rounded-b-lg inline inset-x-0">
+                <h3 class="text-sm font-medium text-slate-50 ">Steve Ben</h3>
               </div>
             </div>
           </div>
