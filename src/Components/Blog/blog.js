a86d287ada4e94b3dc6f36de658ebbf1968836c4
Radi-dev <evaradi18@gmail.com>
2022-03-19 13:36:12 +0100

    fixes

--- src/Components/Blog/blog.js
+++ src/Components/Blog/blog.js
@@ -4,11 +4,20 @@ import Post from "./blogPost";
 import Header from "./blogHeader";
 import Foot from "./blogFooter";
 import About from "./blogAbout";
+import Login from "./blogLogin";
+import Admin from "./blogAdmin";
 import NotFound from "./blog404";
 import Home from "../Home";
-import { Routes, Route, BrowserRouter as Router } from "react-router-dom";
+import {
+  Routes,
+  Route,
+  Navigate,
+  BrowserRouter as Router,
+} from "react-router-dom";
+import { useState } from "react/cjs/react.development";
 
 export default function Blog() {
+  const [loggedIn, setLoggedIn] = useState(true);
   return (
     <div className="mother cursor-default bg-gradient-to-t from-prim2Dark to-prim2 bg-f ixed bg-co h-full font-Comfortaa px-5 py-12">
       <div className="page-conatainer h-full mx-auto w-full relative ">
@@ -27,7 +36,18 @@ export default function Blog() {
               />
             }
           />
-          <Route path="/about" element={<About />} />
+          <Route path="/about" element={<About id="about" />} />
+          <Route path="/login" element={<Login id="login" />} />
+          <Route
+            path="/admin"
+            element={
+              loggedIn ? (
+                <Admin id="admin" />
+              ) : (
+                <Navigate to="/blog/login" replace />
+              )
+            }
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
 
---
+++ src/Components/Blog/blogLogin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Login(d) {
+  return (
+    <div className="  h-screen m-0 sn bg-orange-200 " id={d.id}>
+      <div className="card ">
+        <div className="text-center">Login here</div>
+      </div>
+    </div>
+  );
+}
