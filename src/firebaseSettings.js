b02fd70a34547fa552da2ac51c065fce03e9ce86
Radi-dev <evaradi18@gmail.com>
2022-03-23 18:46:14 +0100

    fix

---
+++ src/firebaseSettings.js
@@ -0,0 +1,10 @@
+//require("dotenv").config();
+
+export const apiKey = process.env.REACT_APP_apiKey;
+export const authDomain = process.env.REACT_APP_authDomain;
+export const databaseURL = process.env.REACT_APP_databaseURL;
+export const projectId = process.env.REACT_APP_projectId;
+export const storageBucket = process.env.REACT_APP_storageBucket;
+export const messagingSenderId = process.env.REACT_APP_messagingSenderId;
+export const appId = process.env.REACT_APP_appId;
+export const measurementId = process.env.REACT_APP_measurementId;
